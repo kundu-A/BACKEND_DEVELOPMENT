e6475d65c0b3f20c216db60338e4561decbcf181
Kundu-A <kunduarpan43@gmail.com>
2024-11-30 10:18:59 +0530

    D-Neuro application initial setup is done

---
+++ D-Neuro/D-Neuro/src/main/java/com/arpankundu/D_Neuro/DNeuroApplication.java
@@ -0,0 +1,13 @@
+package com.arpankundu.D_Neuro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DNeuroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DNeuroApplication.class, args);
+	}
+
+}
---
+++ D-Neuro/D-Neuro/src/test/java/com/arpankundu/D_Neuro/DNeuroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arpankundu.D_Neuro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DNeuroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

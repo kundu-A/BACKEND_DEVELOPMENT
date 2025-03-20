178d68897242016aef606320b5d458b64c49cdc7
Kundu-A <kunduarpan43@gmail.com>
2025-03-20 18:16:18 +0530

    Modify register and login end points and strongly handle exception using try catch

--- DNeuro/DNeuro/src/main/java/com/arpankundu/DNeuro/controllers/PublicController.java
+++ DNeuro/DNeuro/src/main/java/com/arpankundu/DNeuro/controllers/PublicController.java
@@ -1,26 +1,55 @@
 package com.arpankundu.DNeuro.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.arpankundu.DNeuro.models.Users;
-import com.arpankundu.DNeuro.services.UserService;
+import com.arpankundu.DNeuro.repository.PublicRepo;
+import com.arpankundu.DNeuro.services.PublicService;
+import com.arpankundu.DNeuro.services.UtilityService;
+
+import jakarta.validation.Valid;
 
 @RestController
-public class UserController {
+public class PublicController {
 
 	@Autowired
-	private UserService userService;
+	private PublicService userService;
+	
+	@Autowired
+	private UtilityService utilityService;
+	
+	@Autowired
+	private PublicRepo userRepo;
 	
 	@PostMapping("register")
-	public Users register(@RequestBody Users users) {
-		return userService.register(users);
-	}
+	public ResponseEntity<?> register(@RequestBody @Valid Users user) {
+        try {
+            String username = utilityService.extractUsernameFromEmail(user);
+            if (userRepo.findByUsername(username)==null) {
+            	Users record=userService.register(user);
+            	if(record!=null)
+            		return new ResponseEntity<>("Registration successfull!!", HttpStatus.CREATED);
+            }
+            return new ResponseEntity<>("User already exist",HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return new ResponseEntity<>("User Registration Unsuccessful", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
 	@PostMapping("login")
-	public String login(@RequestBody Users users) {
-		return userService.verify(users);
-	}
+	public ResponseEntity<?> login(@RequestBody @Valid Users user) {
+        try {
+            return new ResponseEntity<>(userService.verify(user), HttpStatus.OK);
+        } catch (Exception e) {
+        	System.out.println(e.getMessage());
+            return new ResponseEntity<>("Login Failed...", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
 

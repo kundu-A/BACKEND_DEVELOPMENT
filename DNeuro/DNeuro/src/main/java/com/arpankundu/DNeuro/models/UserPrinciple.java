57a4339f73f01e94bf2e8b0d0f0c116d786a7453
Kundu-A <kunduarpan43@gmail.com>
2025-03-20 18:19:34 +0530

    Modify the getAuthorities method by implementing Role enum instead of string

--- DNeuro/DNeuro/src/main/java/com/arpankundu/DNeuro/models/UserPrinciple.java
+++ DNeuro/DNeuro/src/main/java/com/arpankundu/DNeuro/models/UserPrinciple.java
@@ -18,7 +18,7 @@ public class UserPrinciple implements UserDetails{
 	
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return List.of(new SimpleGrantedAuthority(users.getRole()));
+		return List.of(new SimpleGrantedAuthority(users.getRole().name()));
 	}
 
 	@Override

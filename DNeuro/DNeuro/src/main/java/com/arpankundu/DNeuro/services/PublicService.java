72d9ecfb6a8783e78481ecb6e3ecd0856060947b
Kundu-A <kunduarpan43@gmail.com>
2025-03-20 18:32:34 +0530

    Modify register and login method and add a new method to change older password

--- DNeuro/DNeuro/src/main/java/com/arpankundu/DNeuro/services/PublicService.java
+++ DNeuro/DNeuro/src/main/java/com/arpankundu/DNeuro/services/PublicService.java
@@ -1,33 +1,104 @@
 package com.arpankundu.DNeuro.services;
 
+import java.time.LocalDateTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.arpankundu.DNeuro.components.ChangePassword;
+import com.arpankundu.DNeuro.models.Role;
 import com.arpankundu.DNeuro.models.Users;
-import com.arpankundu.DNeuro.repository.UserRepo;
+import com.arpankundu.DNeuro.repository.PublicRepo;
 
 @Service
-public class UserService {
+public class PublicService {
 
 	@Autowired
-	private UserRepo userRepo;
+	private PublicRepo userRepo;
+	
 	@Autowired
 	private JWTService jwtService;
-	@Autowired
-	AuthenticationManager authenticationManager;
 	
-	private BCryptPasswordEncoder encoder=new BCryptPasswordEncoder(12);
-	public Users register(Users users) {
-		users.setPassword(encoder.encode(users.getPassword()));
-		return userRepo.save(users);
+	@Autowired
+	private UtilityService utilityService;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	private EmailService emailService;
+	
+    @Autowired
+    AuthenticationManager authenticationManager;
+    
+    @Autowired
+    private OTPService otpService;
+	
+	public Users register(Users user) {
+		 String email = user.getEmail();
+	        if (!otpService.verifiedEmails.contains(email)) 
+	            throw new RuntimeException("Email verification required.");
+
+	        if (userRepo.findUsersByEmail(email) != null)
+	            throw new RuntimeException("User with this email already exists.");
+
+	        user.setUsername(utilityService.extractUsernameFromEmail(user));
+	        user.setPassword(passwordEncoder.encode(user.getPassword()));
+	        user.setRole(Role.ROLE_USER);
+	        try {
+	        	Users users=userRepo.save(user);
+	        	if(users!=null) {
+	        		emailService.welcomeEmail(email);
+	        		return users;
+	        	}
+	        	throw new RuntimeException("User registration failed");
+	        }catch(Exception e) {
+	        	throw new RuntimeException("User registration failed", e); 
+	        }
 	}
-	public String verify(Users users) {
-		Authentication authentication=authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(users.getUsername(), users.getPassword()));
-		if(authentication.isAuthenticated())
-			return jwtService.generateToken(users.getUsername());
-		return "Failed to Login!!";
+	public String verify(Users user) {
+		String username=utilityService.extractUsernameFromEmail(user);
+        try {
+        	Authentication authentication =
+        			authenticationManager.authenticate(
+        					new UsernamePasswordAuthenticationToken(
+        							username,
+        							user.getPassword()
+        					)
+        			);
+        	if (authentication.isAuthenticated()) {
+        		String token=jwtService.generateToken(username);
+        		if(token!=null)
+        			user.setTokenIssueTime(LocalDateTime.now());
+        		System.out.println(user.getTokenIssueTime());
+        		return token;
+        	}
+        }catch(Exception e) {
+        	throw new RuntimeException("Invalid username or password.");
+        }
+        throw new RuntimeException("Invalid username or password.");
+    }
+	public boolean changePassword(ChangePassword request) {
+		String newPassword=passwordEncoder.encode(request.getNewPassword());
+		String username=SecurityContextHolder.getContext().getAuthentication().getName();
+		Users user=userRepo.findByUsername(username);
+		if(!passwordEncoder.matches(request.getOlderPassword(), user.getPassword()))
+			return false;
+		if(!request.getNewPassword().equals(request.getConfirmPassword()))
+			return false;
+		try {
+			user.setPassword(newPassword);
+			user.setTokenIssueTime(LocalDateTime.now());
+			userRepo.save(user);
+			return true;
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+			return false;
+		}
 	}
 }

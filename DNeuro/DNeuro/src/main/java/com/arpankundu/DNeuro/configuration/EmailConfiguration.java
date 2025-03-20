b9e20fd1b3de6d1d53d1f8946d4cb455cc9d6350
Kundu-A <kunduarpan43@gmail.com>
2025-03-20 18:46:25 +0530

    Set up is ready to configure the email

---
+++ DNeuro/DNeuro/src/main/java/com/arpankundu/DNeuro/configuration/EmailConfiguration.java
@@ -0,0 +1,29 @@
+package com.arpankundu.DNeuro.configuration;
+
+import java.util.Properties;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class EmailConfiguration {
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername("kunduarpan43@gmail.com");
+        mailSender.setPassword("nxxa vzzp xhgs vqbi");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true"); // Enable debugging logs
+
+        return mailSender;
+	}
+}

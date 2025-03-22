ded611110eeb757e9f9a186c78639dd598c5b824
Kundu-A <kunduarpan43@gmail.com>
2025-03-22 11:31:32 +0530

    Write the body of the controller

--- DNeuro/DNeuro/src/main/java/com/arpankundu/DNeuro/controllers/HealthCheck.java
+++ DNeuro/DNeuro/src/main/java/com/arpankundu/DNeuro/controllers/HealthCheck.java
@@ -1,5 +1,8 @@
 package com.arpankundu.DNeuro.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/public")
 public class HealthCheck {
 
+	@PostMapping("/healt-check")
+	public ResponseEntity<?> health_check(){
+		return new ResponseEntity<>("The server is runnig smoothly!!",HttpStatus.OK);
+	}
 }

fea74b6648edb60c390ee3c0c66e8f1b0a8edd2a
Kundu-A <kunduarpan43@gmail.com>
2025-03-24 20:28:05 +0530

    Some modification is done

--- ENGINEERING_DIGEST/journalApp/journalApp/src/main/java/com/arpankundu/journalApp/controllers/Oauth2Controller.java
+++ ENGINEERING_DIGEST/journalApp/journalApp/src/main/java/com/arpankundu/journalApp/controllers/Oauth2Controller.java
@@ -4,16 +4,24 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/oauth2")
 public class Oauth2Controller {
 
     @GetMapping("/login")
-    public ResponseEntity<?> oauthLogin(@AuthenticationPrincipal OAuth2User user){
-        return new ResponseEntity<>("Hello "+user.getAttribute("name"), HttpStatus.OK);
+    public ResponseEntity<?> oauthLogin(@AuthenticationPrincipal OAuth2User user) {
+        Map<String,Object> response = new HashMap<>();
+        response.put("name",user.getAttribute("name"));
+        response.put("email",user.getAttribute("email"));
+        response.put("picture",user.getAttribute("picture"));
+        return new ResponseEntity<>(response,HttpStatus.OK);
     }
 }

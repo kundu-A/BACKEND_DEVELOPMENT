2b311ce8630bc5a4dc1468101119944eca6cc492
Kundu-A <kunduarpan43@gmail.com>
2025-04-01 12:22:04 +0530

    Add an extra method to find all the notes from database

--- ENGINEERING_DIGEST/journalApp/journalApp/src/main/java/com/arpankundu/journalApp/repository/JournalAppRepo.java
+++ ENGINEERING_DIGEST/journalApp/journalApp/src/main/java/com/arpankundu/journalApp/repository/JournalAppRepo.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.arpankundu.journalApp.models.JournalEntry;
@@ -15,5 +16,10 @@ public interface JournalAppRepo extends JpaRepository<JournalEntry,Integer>{
 	List<JournalEntry> findByUsers(Users user);
 
 	Optional<JournalEntry> findByIdAndUsers(Integer id,Users users);
+
+	@Query(value = "SELECT * FROM journals_list", nativeQuery = true)
+	List<JournalEntry> findAllJournalContent();
+
+
 
 }

189e2dba6d5133ced0b46e600bb4e6c32effb52a
Kundu-A <kunduarpan43@gmail.com>
2025-04-01 12:15:22 +0530

    Create an end points to get all the notes and include pagination concept here

--- ENGINEERING_DIGEST/journalApp/journalApp/src/main/java/com/arpankundu/journalApp/controllers/AdminController.java
+++ ENGINEERING_DIGEST/journalApp/journalApp/src/main/java/com/arpankundu/journalApp/controllers/AdminController.java
@@ -1,13 +1,11 @@
 package com.arpankundu.journalApp.controllers;
 
+import com.arpankundu.journalApp.models.JournalEntry;
+import com.arpankundu.journalApp.models.JournalResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.arpankundu.journalApp.models.Users;
 import com.arpankundu.journalApp.repository.UserRepo;
@@ -15,6 +13,8 @@ import com.arpankundu.journalApp.services.AdminService;
 import com.arpankundu.journalApp.services.UtilityService;
 
 import jakarta.validation.Valid;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin")
@@ -45,5 +45,21 @@ public class AdminController {
     @GetMapping("/get-user-details")
     public ResponseEntity<?> getUserDetails(){
     	return new ResponseEntity<>(adminService.getAllUserDetails(),HttpStatus.OK);
+    }
+
+    //localhost:8080/admin/get-all-notes?pageNumber=0&pageSize=6 , OR
+    //localhost:8080/admin/get-all-notes
+    @GetMapping("/get-all-notes")
+    public  ResponseEntity<?> getAllNotes(
+            @RequestParam(value = "pageNumber",defaultValue = "0",required = false) Integer pageNumber,
+            @RequestParam(value = "pageSize",defaultValue = "3",required = false) Integer pageSize,
+            @RequestParam(value = "sortBy",defaultValue = "id",required = false) String sortBy ,
+            @RequestParam(value = "sortDir",defaultValue = "asc",required = false) String sortDir
+    )
+    {
+        JournalResponse records = adminService.getAllNotes(pageNumber,pageSize,sortBy,sortDir);
+        if (records != null)
+            return new ResponseEntity<>(records, HttpStatus.OK);
+        return new ResponseEntity<>("No Data Found!!",HttpStatus.OK);
     }
 }

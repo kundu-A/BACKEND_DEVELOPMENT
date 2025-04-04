7d7d406aae2644079d6eea260c1b59bae80fa6ba
Kundu-A <kunduarpan43@gmail.com>
2025-04-04 18:00:41 +0530

    This controller is modified by this way where a user can register and logs in from the same end points - But have some issues

--- ENGINEERING_DIGEST/journalApp/journalApp/src/main/java/com/arpankundu/journalApp/controllers/Oauth2Controller.java
+++ ENGINEERING_DIGEST/journalApp/journalApp/src/main/java/com/arpankundu/journalApp/controllers/Oauth2Controller.java
@@ -1,5 +1,9 @@
 package com.arpankundu.journalApp.controllers;
 
+import com.arpankundu.journalApp.models.Users;
+import com.arpankundu.journalApp.services.*;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -17,12 +21,41 @@ import java.util.Map;
 @CrossOrigin(origins = "http://localhost:3000")
 public class Oauth2Controller {
 
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    UtilityService utilityService;
+
+    @Autowired
+    MyUserServiceDetails userDetailsService;
+
+    @Autowired
+    EmailService emailService;
+
+    @Autowired
+    JwtService jwtService;
+
     @GetMapping("/login")
-    public ResponseEntity<?> oauthLogin(@AuthenticationPrincipal OAuth2User user) {
-        Map<String,Object> response = new HashMap<>();
-        response.put("name",user.getAttribute("name"));
-        response.put("email",user.getAttribute("email"));
-        response.put("picture",user.getAttribute("picture"));
-        return new ResponseEntity<>(response,HttpStatus.OK);
+    public ResponseEntity<?> oauthLogin(@AuthenticationPrincipal OAuth2User user , HttpServletResponse res) {
+        String email = user.getAttribute("email");
+        String username = utilityService.extractUsernameFromEmail(email);
+        try {
+            userDetailsService.loadUserByUsername(username);
+        } catch (Exception e) {
+            Users newUser = userService.oauthRegister(user);
+            if (newUser != null)
+                emailService.welcomeEmail(email);
+        }
+        String accessToken = jwtService.generateToken(username);
+        String refreshToken=jwtService.generateRefreshToken(username);
+        Map<String, Object> response = new HashMap<>();
+        response.put("name", user.getAttribute("name"));
+        response.put("email", user.getAttribute("email"));
+        response.put("picture", user.getAttribute("picture"));
+        response.put("jwt_token", accessToken);
+        userService.setCookie(res, "Access-Token", accessToken, 60 * 60);
+        userService.setCookie(res, "Refresh-Token", refreshToken, 7 * 24 * 60 * 60);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }

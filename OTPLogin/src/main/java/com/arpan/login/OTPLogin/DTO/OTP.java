1e3daf1da804afdf8938d62b23ad2b592da63f0e
Kundu-A <kunduarpan43@gmail.com>
2025-04-14 19:09:29 +0530

    This is a DTO to access OTP and phone number

---
+++ OTPLogin/src/main/java/com/arpan/login/OTPLogin/DTO/OTP.java
@@ -0,0 +1,26 @@
+package com.arpan.login.OTPLogin.DTO;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class OTP {
+
+    private String phoneNumber;
+    private String otp;
+
+    public String getOtp() {
+        return otp;
+    }
+
+    public void setOtp(String otp) {
+        this.otp = otp;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}

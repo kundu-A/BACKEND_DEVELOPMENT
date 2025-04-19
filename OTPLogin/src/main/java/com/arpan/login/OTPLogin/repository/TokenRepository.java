01f4ccc35e7bc8c14f3c33a22f53c63f201feb32
Kundu-A <kunduarpan43@gmail.com>
2025-04-19 18:27:15 +0530

    Token related work like save , delete ,etc will be handled by this repository

---
+++ OTPLogin/src/main/java/com/arpan/login/OTPLogin/repository/TokenRepository.java
@@ -0,0 +1,10 @@
+package com.arpan.login.OTPLogin.repository;
+
+import com.arpan.login.OTPLogin.models.OneTimeToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TokenRepository extends JpaRepository<OneTimeToken,Integer> {
+    OneTimeToken findOneTimeTokenByPhoneNumber(String phoneNumber);
+}

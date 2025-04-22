37862982b5f9a1676d9accc916679527a216ff7a
Kundu-A <kunduarpan43@gmail.com>
2025-04-22 18:28:32 +0530

    Create new controller related to student by which we can set and get student details and make end points

---
+++ OTPLogin/src/main/java/com/arpan/login/OTPLogin/controller/StudentController.java
@@ -0,0 +1,41 @@
+package com.arpan.login.OTPLogin.controller;
+
+import com.arpan.login.OTPLogin.DTO.StudentDTO;
+import com.arpan.login.OTPLogin.models.Students;
+import com.arpan.login.OTPLogin.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+public class StudentController {
+
+    @Autowired
+    StudentService studentService;
+
+    @PostMapping("/set-student-details")
+    public ResponseEntity<?> setStudentDetails(@RequestBody StudentDTO studentDTO){
+        try{
+            Students response= studentService.setStudentDetails(studentDTO);
+            if(response!=null)
+                return new ResponseEntity<>(response,HttpStatus.OK);
+            return new ResponseEntity<>("May be the student record is found",HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return new ResponseEntity<>("Some Internal issues", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/get-student-details/{studentId}")
+    public ResponseEntity<?> getStudentDetails(@PathVariable Integer studentId){
+        try{
+            Students students= studentService.getStudentDetails(studentId);
+            return new ResponseEntity<>(students,HttpStatus.OK);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return new ResponseEntity<>("Some Internal Issues",HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}

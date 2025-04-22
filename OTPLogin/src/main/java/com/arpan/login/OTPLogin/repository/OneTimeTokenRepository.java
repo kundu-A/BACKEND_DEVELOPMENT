09e3f00052c123cfdf45eaf67d26cee6d6ed0dfb
Kundu-A <kunduarpan43@gmail.com>
2025-04-22 18:44:57 +0530

    Change the name from token to one to token

--- OTPLogin/src/main/java/com/arpan/login/OTPLogin/repository/OneTimeTokenRepository.java
+++ OTPLogin/src/main/java/com/arpan/login/OTPLogin/repository/OneTimeTokenRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TokenRepository extends JpaRepository<OneTimeToken,Integer> {
+public interface OneTimeTokenRepository extends JpaRepository<OneTimeToken,Integer> {
     OneTimeToken findOneTimeTokenByPhoneNumber(String phoneNumber);
 }

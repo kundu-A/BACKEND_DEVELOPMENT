8e189f57596d22bc44e755d944db897f3a775baf
Kundu-A <kunduarpan43@gmail.com>
2025-04-22 18:35:21 +0530

    Create a repository to fetch details from the database

---
+++ OTPLogin/src/main/java/com/arpan/login/OTPLogin/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.arpan.login.OTPLogin.repository;
+
+import com.arpan.login.OTPLogin.models.Students;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Students,Integer> {
+}

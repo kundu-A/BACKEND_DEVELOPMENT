5723add42bf55c17c6a7b7fe51a0ec5e01b22f72
Kundu-A <kunduarpan43@gmail.com>
2025-04-22 18:42:41 +0530

    Add an extra checking that current time and saved time

--- OTPLogin/src/main/java/com/arpan/login/OTPLogin/config/JWTAuthenticationFilter.java
+++ OTPLogin/src/main/java/com/arpan/login/OTPLogin/config/JWTAuthenticationFilter.java
@@ -1,5 +1,7 @@
 package com.arpan.login.OTPLogin.config;
 
+import com.arpan.login.OTPLogin.models.Tokens;
+import com.arpan.login.OTPLogin.repository.TokenRepository;
 import com.arpan.login.OTPLogin.service.JWTService;
 import com.arpan.login.OTPLogin.service.MyUserServiceDetails;
 import jakarta.servlet.FilterChain;
@@ -15,6 +17,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Date;
 
 @Component
 public class JWTAuthenticationFilter extends OncePerRequestFilter {
@@ -25,26 +28,37 @@ public class JWTAuthenticationFilter extends OncePerRequestFilter {
     @Autowired
     MyUserServiceDetails myUserServiceDetails;
 
+    @Autowired
+    TokenRepository tokenRepository;
+
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String authHeader= request.getHeader("Authorization");
-        String phoneNumber=null;
-        String token=null;
-        if(authHeader!=null){
-            if(authHeader.startsWith("Bearer ")){
-                token=authHeader.substring(7);
-                phoneNumber= jwtService.extractPhoneNumber(token);
-            }
-            if (phoneNumber != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-                UserDetails userDetails = myUserServiceDetails.loadUserByPhoneNumber(phoneNumber);
-                if (jwtService.validateAccessToken(token, userDetails)) {
-                    UsernamePasswordAuthenticationToken authToken =
-                            new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                    SecurityContextHolder.getContext().setAuthentication(authToken);
+        try {
+            String authHeader = request.getHeader("Authorization");
+            String phoneNumber = null;
+            String token = null;
+            if (authHeader != null) {
+                if (authHeader.startsWith("Bearer ")) {
+                    token = authHeader.substring(7);
+                    phoneNumber = jwtService.extractPhoneNumber(token);
+                }
+                Tokens tokens = tokenRepository.findTokensByAccessToken(token);
+                if (System.currentTimeMillis()<tokens.getExpirationDuration()) {
+                    if (phoneNumber != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                        UserDetails userDetails = myUserServiceDetails.loadUserByPhoneNumber(phoneNumber);
+                        if (jwtService.validateAccessToken(token, userDetails)) {
+                            UsernamePasswordAuthenticationToken authToken =
+                                    new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                            authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                            SecurityContextHolder.getContext().setAuthentication(authToken);
+                        }
+                    }
                 }
             }
+            filterChain.doFilter(request, response);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
-        filterChain.doFilter(request, response);
     }
 }

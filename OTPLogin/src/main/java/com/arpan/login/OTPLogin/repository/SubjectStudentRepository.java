c87820e223659beaa77c97d869408ea0c3b4cd59
Kundu-A <kunduarpan43@gmail.com>
2025-04-23 18:31:40 +0530

    Create a new Repo to access SubjectStudent table's data

---
+++ OTPLogin/src/main/java/com/arpan/login/OTPLogin/repository/SubjectStudentRepository.java
@@ -0,0 +1,9 @@
+package com.arpan.login.OTPLogin.repository;
+
+import com.arpan.login.OTPLogin.models.SubjectStudent;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SubjectStudentRepository extends JpaRepository<SubjectStudent,Integer> {
+}

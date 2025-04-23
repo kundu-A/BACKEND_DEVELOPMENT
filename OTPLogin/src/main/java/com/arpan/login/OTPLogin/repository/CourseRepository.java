d80a292fe9e5fab6f3b4d1da299134312cca78a5
Kundu-A <kunduarpan43@gmail.com>
2025-04-23 18:38:45 +0530

    Add new two method - one is for checking the course is present or not using course name , another one is to find subject list using course id

--- OTPLogin/src/main/java/com/arpan/login/OTPLogin/repository/CourseRepository.java
+++ OTPLogin/src/main/java/com/arpan/login/OTPLogin/repository/CourseRepository.java
@@ -1,11 +1,20 @@
 package com.arpan.login.OTPLogin.repository;
 
 import com.arpan.login.OTPLogin.models.Course;
+import com.arpan.login.OTPLogin.models.Subjects;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course,Integer> {
 
     Course findCourseByCourseName(String courseName);
+    boolean existsByCourseName(String courseName);
+
+    @Query("select cs.subjects from CourseSubject cs where cs.course.id = :courseId")
+    List<Subjects> findSubjectsByCourseId(@Param("courseId") Integer courseId);
 }

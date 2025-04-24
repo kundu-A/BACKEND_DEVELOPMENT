bdb717d50a000657c9300e9d88dbd1a9f5889cb8
Kundu-A <kunduarpan43@gmail.com>
2025-04-24 18:46:34 +0530

    Create a method to fetch all the students related to subject

--- OTPLogin/src/main/java/com/arpan/login/OTPLogin/service/StudentService.java
+++ OTPLogin/src/main/java/com/arpan/login/OTPLogin/service/StudentService.java
@@ -1,12 +1,14 @@
 package com.arpan.login.OTPLogin.service;
 
 import com.arpan.login.OTPLogin.DTO.StudentDTO;
-import com.arpan.login.OTPLogin.models.Course;
-import com.arpan.login.OTPLogin.models.Students;
-import com.arpan.login.OTPLogin.repository.CourseRepository;
-import com.arpan.login.OTPLogin.repository.StudentRepository;
+import com.arpan.login.OTPLogin.DTO.SubjectDTO;
+import com.arpan.login.OTPLogin.models.*;
+import com.arpan.login.OTPLogin.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class StudentService {
@@ -17,16 +19,43 @@ public class StudentService {
     @Autowired
     StudentRepository studentRepository;
 
+    @Autowired
+    SubjectRepository subjectRepository;
+
+    @Autowired
+    CourseSubjectRepository courseSubjectRepository;
+
+    @Autowired
+    SubjectStudentRepository subjectStudentRepository;
+
     public Students setStudentDetails(StudentDTO studentDTO){
+        if(!courseRepository.existsByCourseName(studentDTO.getCourseName()))
+            return null;
         Course course= courseRepository.findCourseByCourseName(studentDTO.getCourseName());
+        List<CourseSubject> courseSubjects=courseSubjectRepository.findAllByCourseId(course.getId());
         Students students=new Students();
         students.setName(studentDTO.getName());
         students.setRollNumber(studentDTO.getRollNumber());
         students.setCourse(course);
-        return studentRepository.save(students);
+        studentRepository.save(students);
+        for(CourseSubject cs: courseSubjects){
+            SubjectStudent subjectStudent=new SubjectStudent();
+            subjectStudent.setStudents(students);
+            subjectStudent.setSubjects(cs.getSubjects());
+            subjectStudentRepository.save(subjectStudent);
+        }
+        return students;
     }
 
     public Students getStudentDetails(Integer studentId){
         return studentRepository.findById(studentId).orElseThrow(()->new RuntimeException("Not Found"));
+    }
+
+    public List<Subjects> fetchSubjectListUsingStudentRollNumber(String rollNumber){
+        Students students=studentRepository.findStudentByRollNumber(rollNumber);
+        List<SubjectStudent> subjectStudents=subjectStudentRepository.findAllByStudentsId(students.getId());
+        return subjectStudents.stream()
+                .map(SubjectStudent::getSubjects)
+                .toList();
     }
 }

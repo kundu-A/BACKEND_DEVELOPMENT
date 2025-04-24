031920f3a98a56cc51831c576ffa9aedde3e5084
Kundu-A <kunduarpan43@gmail.com>
2025-04-24 18:45:30 +0530

    Create two methods to return SubjectStudent object by using student id in one and in another using subject id

--- OTPLogin/src/main/java/com/arpan/login/OTPLogin/repository/SubjectStudentRepository.java
+++ OTPLogin/src/main/java/com/arpan/login/OTPLogin/repository/SubjectStudentRepository.java
@@ -4,6 +4,10 @@ import com.arpan.login.OTPLogin.models.SubjectStudent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SubjectStudentRepository extends JpaRepository<SubjectStudent,Integer> {
+    List<SubjectStudent> findAllByStudentsId(Integer studentId);
+    List<SubjectStudent> findAllBySubjectsId(Integer subjectId);
 }

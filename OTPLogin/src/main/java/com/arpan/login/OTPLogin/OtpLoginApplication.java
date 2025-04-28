636d3657f901ff1be69310cecce9370d1cf7bbfe
Kundu-A <kunduarpan43@gmail.com>
2025-04-28 18:33:49 +0530

    Some modification is done here

--- OTPLogin/src/main/java/com/arpan/login/OTPLogin/OtpLoginApplication.java
+++ OTPLogin/src/main/java/com/arpan/login/OTPLogin/OtpLoginApplication.java
@@ -1,13 +1,20 @@
 package com.arpan.login.OTPLogin;
 
+import com.arpan.login.OTPLogin.service.RedisTest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class OtpLoginApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OtpLoginApplication.class, args);
+		//SpringApplication.run(OtpLoginApplication.class, args);
+		ConfigurableApplicationContext context=SpringApplication.run(OtpLoginApplication.class, args);
+		RedisTest bean = context.getBean(RedisTest.class);
+		System.out.println("Hiii");
+		bean.redisTest();
 	}
 
 }

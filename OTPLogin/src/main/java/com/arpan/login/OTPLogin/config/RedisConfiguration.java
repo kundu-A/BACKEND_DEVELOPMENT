23da54d9f4507ed5787a6ee4facbd23bde9931ad
Kundu-A <kunduarpan43@gmail.com>
2025-04-29 19:05:06 +0530

    Add a new bean to configure RedisConnectionFactory

--- OTPLogin/src/main/java/com/arpan/login/OTPLogin/config/RedisConfiguration.java
+++ OTPLogin/src/main/java/com/arpan/login/OTPLogin/config/RedisConfiguration.java
@@ -1,13 +1,26 @@
 package com.arpan.login.OTPLogin.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfiguration {
+
+    @Value("${spring.redis.host}")
+    private String hostName;
+
+    @Value("${spring.redis.port}")
+    private String port;
+
+    @Value("${spring.redis.password}")
+    private String password;
 
     @Bean
     public RedisTemplate<String ,String> redisTemplate(RedisConnectionFactory factory){
@@ -18,5 +31,14 @@ public class RedisConfiguration {
         redisTemplate.setValueSerializer(new StringRedisSerializer());
 
         return redisTemplate;
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(){
+        RedisStandaloneConfiguration configuration=new RedisStandaloneConfiguration();
+        configuration.setHostName(hostName);
+        configuration.setPort(Integer.parseInt(port));
+        configuration.setPassword(password);
+        return new LettuceConnectionFactory(configuration);
     }
 }
